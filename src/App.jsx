a64c58ade724316ce3d017040a6ea65e4d7dd310
Estela Extremera <estelaextremeracampos@gmail.com>
2023-11-28 13:53:17 +0100

    add formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
+import {  Typography, Container, Grid } from '@mui/material';
+import Formulario from './components/Formulario';
 
 function App() {
 
   return (
-    
-      <div>
-        
-        <h1>App Noticias</h1>
-    </div>
-  )
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
+          Buscador de Noticias
+        </Typography>
+      </header>
+      <Grid>
+        <Grid item md={6}>
+          <Formulario />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App
